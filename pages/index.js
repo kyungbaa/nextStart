552c6163a22d7a75d237c482ac68d7411edd7f99
kyungbaa <cocoroto@naver.com>
2023-01-03 10:39:28 +0900

    Link 컴포넌트로 네비게이팅하기

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <div className={styles.main}>
-        <h1>next Js Start</h1>
+      <div>
+        <h1>The Home Page</h1>
+        <ul>
+          <li>
+            <a href="/portfolio">Portfolio</a>
+            {/* 기존 a태그 사용시 표준 링크가 주는 문제점은 새페이지를 불러오기 위해 새 http요청을 했을때, 
+            react앱을 실행할 때 갖는 앱 상태 (state)가 바뀌는 상황이 생김 react context redux
+            next만 해당되는거 아님 리액트 사용에 적합하지 않음
+            */}
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link replace href="/portfolio">
+              Portfolio
+            </Link>
+            {/* replace -> push대신 replace하는 속성 뒤로가기 history에 쌓이지 않고 대체된다 */}
+          </li>
+          <li>
+            <Link href="/client">Clients</Link>
+          </li>
+        </ul>
       </div>
     </>
   );
