17c7557ab32df09a73a81da2fda7f929f1a45430
kyungbaa <cocoroto@naver.com>
2023-01-03 12:31:29 +0900

    프로그래밍 방식으로 내비게이팅

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,19 @@ import { useRouter } from 'next/router';
 export default function ClientProjectspage() {
   const router = useRouter();
   console.log('query', router.query); // {id: 'macc'}
+  const loadProjectHendler = () => {
+    // load data...
+    // router.push('/clients/max/projecta');
+    //   router.replace('/clients/max/projecta'); 뒤로 돌아가지 못하게 현재 페이지 대체
+    router.push({
+      pathname: '/clients/[id]/[clientProjectId]',
+      query: { id: 'max', clientProjectId: 'projecta' },
+    });
+  };
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHendler}>Load Project A</button>
     </div>
   );
 }
